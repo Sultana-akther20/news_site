d465d5dc984d5c1733549dfead55433091ea6215
Sultana-akther20 <Sultana.akther2805@gmail.com>
2025-05-07 00:24:35 +0100

    Add all the files and folders in my project and add the code for setup

--- news/settings.py
+++ news/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-iq6+orl7xwj!rx8c9jq1&+ms$d_r7crovk+80q4@s97#-$%xzr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR / 'assets')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
