b6920bcc665a25a708a6dee21a12df7cf70eda0c
Sultana-akther20 <Sultana.akther2805@gmail.com>
2025-05-07 15:34:17 +0100

    Add all the files and folders and all the codes

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_2hoAPUJk8FIK@ep-sweet-math-a2v1vsga.eu-central-1.aws.neon.tech/knelt_arena_rigor_773563?sslmode=require"
+)
