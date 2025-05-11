4559ada31350f2e87bce87d1a3255b885920e7a3
Sultana-akther20 <Sultana.akther2805@gmail.com>
2025-05-11 21:58:50 +0100

    Add the posts.json improvements and new templates path

--- newstic/models.py
+++ newstic/models.py
@@ -53,25 +53,3 @@ class Comment(models.Model):
     def __str__(self):
         return f"Comment by {self.author.username} on {self.post.title}"
     
-"""class Post(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True, blank=True)    
-    content = models.TextField()
-    excerpt = models.TextField(max_length=200, blank=True)  
-    created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    status = models.CharField(max_length=10, default='draft', choices=[
-        ('draft', 'Draft'),
-        ('published', 'Published')
-    ])
-    total_likes = models.IntegerField(default=0)
-    total_dislikes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.title  
-
-    def save(self, *args, **kwargs):
-        if not self.excerpt:
-            self.excerpt = self.content[:200] + '...' if len(self.content) > 200 else self.content
-        super().save(*args, **kwargs)  
-        """
