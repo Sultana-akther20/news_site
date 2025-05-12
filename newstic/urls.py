d1fe8fbda0ff510e7cf583a2e578bdcdd4aa5ea1
Sultana-akther20 <Sultana.akther2805@gmail.com>
2025-05-12 23:41:45 +0100

    Add the copyright html code

--- newstic/urls.py
+++ newstic/urls.py
@@ -1,17 +1,7 @@
-"""from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.ArticleList.as_view(), name='home'),
-]
-"""
 from . import views
 from django.urls import path
 
 urlpatterns = [
     path('', views.ArticleList.as_view(), name='home'),
-    path('article/<slug:slug>/', views.ArticleDetail.as_view(), name='article_detail'),
-    path('like/<slug:slug>/', views.article_like, name='article_like'),
-    path('dislike/<slug:slug>/', views.article_dislike, name='article_dislike'),
-    path('article/<slug:slug>/comment/', views.add_comment, name='add_comment'),
+  
 ]
