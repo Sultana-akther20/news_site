86c1823a4071ad1e341bd9f9d7ecbff92b3e2634
Sultana-akther20 <Sultana.akther2805@gmail.com>
2025-05-13 14:07:47 +0100

    Add the deletion of secret key and moving the gitignore to root directory

--- news/settings.py
+++ news/settings.py
@@ -22,9 +22,6 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iq6+orl7xwj!rx8c9jq1&+ms$d_r7crovk+80q4@s97#-$%xzr'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
