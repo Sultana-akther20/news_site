e73aba690fdf2ee2751242175ece8adcfa8b9e99
Sultana-akther20 <Sultana.akther2805@gmail.com>
2025-05-15 11:41:46 +0100

    Remove env.py in favor of .env

--- env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_2hoAPUJk8FiK@ep-sweet-math-a2v1vsga.eu-central-1.aws.neon.tech/knelt_arena_rigor_773563?sslmode=require"
-)
-os.environ.setdefault("SECRET_KEY", "Sultana20?Akther")
-
-
-os.environ.setdefault('NEWS_API_KEY', 'a4e5c2ad9a564f55b74c6308aaee5824')
